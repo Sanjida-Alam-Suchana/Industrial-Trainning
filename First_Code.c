ee7fa2466a163265bc7355fb94d1c6bd4e01843f
Your Name <your_email@example.com>
2025-01-27 18:13:06 +0600

    print second fibonacci code

--- First_Code.c
+++ First_Code.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
-int main() {
-    
-    for (int i = 1; i <= 10; i++) {
-        printf("%d\n", i);
+int main() 
+{
+    int n, first = 0, second = 1, next;
+
+    // Ask the user for the number of terms
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (int i = 0; i < n; i++) {
+        if (i <= 1) {
+            next = i; // First two terms are 0 and 1
+        } else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%d ", next);
     }
+
+    printf("\n");
 
     return 0;
 }
